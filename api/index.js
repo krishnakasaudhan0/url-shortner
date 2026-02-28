a072c52505dec3a881c670d9fb9a52980cfb2642
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-03-01 03:25:53 +0530

    feat: restored server.js api consumption

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-// const app = require('../server.js');
-
-// module.exports = app;
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server is working on Vercel");
-});
+const app = require('../server.js');
 
 module.exports = app;
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is working on Vercel");
+// });
+
+// module.exports = app;
